1272b26b56ef930d4f2d4b118a74e3905730b622
ryuhei0204 <darvish0204@gmail.com>
2019-04-19 16:44:00 +0900

    4/19

---
+++ TestProject01/src/HelloWorld2.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld2 {
+
+	public static void main(String[] args) {
+		System.out.println(gokei(2,3));
+	}
+public static int gokei(int number1,int number2){
+return number1+number2;
+}
+}
